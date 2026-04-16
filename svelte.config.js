89fd051f459a33f0a855b44a0dada3ed029a7ad8
jessieyyt <131130380+jessieyyt@users.noreply.github.com>
2026-04-15 22:24:10 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,15 +8,3 @@ const config = {
 };
 
 export default config;
-src/app.html
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>SWA Header Injection Demo</title>
-</head>
-<body>
-  <div>%sveltekit.body%</div>
-</body>
-</html>
